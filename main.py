6d5e3184da52cfa519573bc079b1054d20fa800d
Sergey <thextreme.30@gmail.com>
2022-07-26 15:25:30 +0600

    created upload images func

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import shutil
+from typing import List
 from fastapi import FastAPI, UploadFile, File
 
 
@@ -9,4 +10,10 @@ app = FastAPI()
 async def upload_video(file: UploadFile = File(...)):
     with open(f'{file.filename}', 'wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
-    return {'file_name': file.filename}
+
+
+@app.post('/image')
+async def upload_image(files: List[UploadFile] = File(...)):
+    for image in files:
+        with open(f'{image.filename}', 'wb') as buffer:
+            shutil.copyfileobj(image.file, buffer)
